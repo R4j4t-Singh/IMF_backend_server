fc3b6ee3fc98b04f44c53105162eb16334181762
R4j4t-Singh <202011061@diu.iiitvadodara.ac.in>
2025-02-01 17:27:29 +0530

    added PORT config for server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv/config";
 import app from "./app.js";
 
-const PORT = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT || 3000, () => {
-  console.log(`Listening on ${PORT}`);
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
 });
